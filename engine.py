36e46de433e6a68e82892540e42c95d6269382cb
Wahab H <wahhab.eisa+GITHUB@gmail.com>
2026-05-18 16:03:47 +0100

    Created classes to store objects and methods

--- engine.py
+++ engine.py
@@ -1,5 +1,17 @@
 # The most functional file "engine/config/operating", also most important
 
+# Importing essential libraries for various purposes
+from random import randint # randint -> generates a random number from a given range
+
 # start() will be used to launch the CLI booking demo
 def start(): # Initializing the function
-    print(True) # Testing if it prints the boolean "True", to determine a proper result
+    pass # Place Holder
+
+class Airplane: # Initializing a Class to manage demo user details and booking
+    def __init__(self, reference, surname): # Initializing object attributes
+        self.reference = reference # "Reference" -> Reference number linked to booking
+        self.surname = surname # "Surname" -> User Surname for verification
+
+class Operate: # Initializing a Class to contain several functional methods
+    def generate(): # Initializing a method to generate a unique reference number
+        pass # Place Holder

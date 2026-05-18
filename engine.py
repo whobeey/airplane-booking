560b2d0d63f41ba74d242f7a547346a9117d295e
Wahab H <wahhab.eisa+GITHUB@gmail.com>
2026-05-18 15:19:28 +0100

    Finalized main.py and created engine.py

---
+++ engine.py
@@ -0,0 +1,5 @@
+# The most functional file "engine/config/operating", also most important
+
+# start() will be used to launch the CLI booking demo
+def start(): # Initializing the function
+    print(True) # Testing if it prints the boolean "True", to determine a proper result
---
+++ main.py
@@ -0,0 +1,6 @@
+# Import the start function from the engine module
+from engine import start as main # Set alias to "main"
+
+# If this file is run, and it has the name "main.py", then call main()
+if __name__ == "__main__": # If this statement is true, do the following:
+    main() # main takes no arguments, because it has no parameters
